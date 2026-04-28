085c721ae9c0838fcd9d44a437e142273c82d678
rniedziolkad <rafal.niedziolka-domanski@mail.umcs.pl>
2026-04-28 18:10:55 +0200

    testy

--- generyczne/src/Main.java
+++ generyczne/src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        CustomList customList = new CustomList();
+        customList.addLast(0);
+        customList.addLast(1);
+        customList.addLast(2);
+        customList.addLast(3);
+        customList.addFirst(100);
 
+        System.out.println(customList.getFirst());
+        System.out.println(customList.getLast());
+        System.out.println("usunięto: " + customList.removeFirst());
+        System.out.println("usunięto: " + customList.removeLast());
+        System.out.println(customList.getFirst());
+        System.out.println(customList.getLast());
     }
 }
