120b1091275cd265f0a1f3dd2744816e6a27d39f
rniedziolkad <rafal.niedziolka-domanski@mail.umcs.pl>
2026-05-05 18:13:52 +0200

    digital clock

--- K_24/src/DigitalClock.java
+++ K_24/src/DigitalClock.java
@@ -1,2 +1,32 @@
-public class DigitalClock {
+public class DigitalClock extends Clock {
+    public enum Type {
+        H24, H12;
+    }
+
+    private Type clockType;
+    public DigitalClock(Type clockType) {
+        this.clockType = clockType;
+    }
+
+    @Override
+    public String toString() {
+        if (clockType == Type.H24) {
+            return super.toString();
+
+        } else {
+            if (hours == 0) {
+                return String.format("%d:%02d:%02d AM", 12, min, sec);
+            }
+
+            if (hours == 12) {
+                return String.format("%d:%02d:%02d PM", 12, min, sec);
+            }
+            if (hours > 12 && hours < 24) {
+                return String.format("%d:%02d:%02d PM", hours - 12, min, sec);
+            }
+            else {
+                return String.format("%d:%02d:%02d AM", hours, min, sec);
+            }
+        }
+    }
 }
--- K_24/src/Main.java
+++ K_24/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
+        DigitalClock clock = new DigitalClock(DigitalClock.Type.H12);
+        clock.setTime(1, 0, 0);
+        System.out.println("Czas: " + clock.toString());
+
     }
 }
