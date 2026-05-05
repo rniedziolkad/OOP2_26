3dd4866996610e1ce183f60f885d9692181cac50
rniedziolkad <rafal.niedziolka-domanski@mail.umcs.pl>
2026-05-05 17:57:31 +0200

    Clock

---
+++ K_24/src/Clock.java
@@ -0,0 +1,34 @@
+import java.time.LocalTime;
+
+public abstract class Clock {
+    protected int hours;
+    protected int min;
+    protected int sec;
+
+    public void setCurrentTime(){
+        LocalTime currentTime = LocalTime.now();
+        this.hours = currentTime.getHour();
+        this.min = currentTime.getMinute();
+        this.sec = currentTime.getSecond();
+    }
+
+    public void setTime(int hours, int min, int sec){
+        if(hours >= 24 || hours <= -1){
+            throw new IllegalArgumentException("Godzina nie poprawna");
+        }
+        if(min >= 60 || min <= -1){
+            throw new IllegalArgumentException("Minuty nie poprawne");
+        }
+        if(sec >= 60 || sec <= -1){
+            throw new IllegalArgumentException("Secundy nie poprawne");
+        }
+        this.hours = hours;
+        this.min = min;
+        this.sec = sec;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%02d:%02d:%02d", hours, min, sec);
+    }
+}
