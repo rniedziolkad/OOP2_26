62b75d14d7d3ea60965db05f72bf6bce01940040
rniedziolkad <rafal.niedziolka-domanski@mail.umcs.pl>
2026-05-05 18:22:50 +0200

    city

---
+++ K_24/src/City.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+
+public class City {
+
+    private String capital;
+    private int timezone;
+    private String lat;
+    private String lon;
+
+    public City(String capital, int timezone, String lat, String lon) {
+        this.capital = capital;
+        this.timezone = timezone;
+        this.lat = lat;
+        this.lon = lon;
+    }
+
+    private static City parseLine(String line) {
+        String[] part = line.split(",", 4);
+        if(part.length != 4) throw new IllegalArgumentException();
+
+        return new City(part[0], Integer.parseInt(part[1]), part[2].strip(), part[3].strip());
+    }
+
+    public static Map<String, City> parseFile(String path) {
+        Map<String, City> cities = new HashMap<>();
+        try(BufferedReader reader = Files.newBufferedReader(Path.of(path))) {
+            reader.readLine();
+            String line;
+            while((line = reader.readLine()) != null) {
+                City city = parseLine(line);
+                cities.put(city.capital, city);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return cities;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "capital='" + capital + '\'' +
+                ", timezone=" + timezone +
+                ", lat='" + lat + '\'' +
+                ", lon='" + lon + '\'' +
+                '}';
+    }
+}
--- K_24/src/Main.java
+++ K_24/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -5,5 +7,7 @@ public class Main {
         clock.setTime(1, 0, 0);
         System.out.println("Czas: " + clock.toString());
 
+        Map<String, City> cities = City.parseFile("strefy.csv");
+        System.out.println(cities.toString());
     }
 }
