6dffa896b12e26b2e8fcdd04c990ed4d8d10ab5d
rniedziolkad <rafal.niedziolka-domanski@mail.umcs.pl>
2026-05-19 18:15:44 +0200

    łączenie z bazą danych

--- site/src/main/java/pl/umcs/oop/Main.java
+++ site/src/main/java/pl/umcs/oop/Main.java
@@ -1,8 +1,38 @@
 package pl.umcs.oop;
 
 
+import pl.umcs.oop.database.DatabaseConnection;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        DatabaseConnection.connect("test.db");
+        Statement stmt = DatabaseConnection.getConnection().createStatement();
+        stmt.execute("""
+            CREATE TABLE IF NOT EXISTS account (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT UNIQUE,
+                password TEXT
+            )
+        """);
+
+        stmt.execute("""
+            INSERT OR IGNORE INTO account (username, password)
+            VALUES ("user1", "pass1"),
+                   ("user2", "pass2"),
+                   ("user3", "pass3");
+        """);
+
+        ResultSet rs = stmt.executeQuery("""
+            SELECT * FROM account;
+        """);
+
+        while (rs.next()) {
+            System.out.println(rs.getString("username") + ": " + rs.getString("password") + " " + rs.getInt("id"));
+        }
 
     }
 }
