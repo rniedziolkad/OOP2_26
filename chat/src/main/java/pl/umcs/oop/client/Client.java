b16c4fc37215628b09ad97b2d3ba0f93f8a47668
rniedziolkad <rafal.niedziolka-domanski@mail.umcs.pl>
2026-06-02 18:04:53 +0200

    klient

---
+++ chat/src/main/java/pl/umcs/oop/client/Client.java
@@ -0,0 +1,27 @@
+package pl.umcs.oop.client;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) {
+        // do komunikacji z serwerem służy obiekt klasy Socket
+        try (Socket socket = new Socket("localhost", 12345)) {
+            System.out.println("Połączono z serwerem");
+            Scanner in = new Scanner(socket.getInputStream());  // do odczytywania wiadomości z serwera
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);  // do wysyłania wiadomości
+            Scanner consoleIn = new Scanner(System.in); // System.in to InputStream z konsoli
+            String input = consoleIn.nextLine();
+            while (!input.equals("exit")) { // ustaliliśmy, że słowo exit kończy działanie
+                out.println(input);     // wysyłanie wczytanej linii
+                String message = in.nextLine(); // odczyt wiadomości z serwera
+                System.out.println(message);
+                input = consoleIn.nextLine();   // czytamy kolejne wejscie z konsoli
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
