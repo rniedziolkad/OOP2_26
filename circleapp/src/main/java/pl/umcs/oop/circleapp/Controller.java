85ee63ab78feddefd8d11960ba28d8d4c65b73c8
rniedziolkad <rafal.niedziolka-domanski@mail.umcs.pl>
2026-06-09 15:57:59 +0200

    circle app

--- circleapp/src/main/java/pl/umcs/oop/circleapp/Controller.java
+++ circleapp/src/main/java/pl/umcs/oop/circleapp/Controller.java
@@ -5,6 +5,9 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 
 public class Controller {
     @FXML
@@ -27,7 +30,21 @@ public class Controller {
         // TODO
     }
     @FXML
-    protected void onMouseClicked() {
-        // TODO
+    protected void onMouseClicked(MouseEvent mouseEvent) {
+        // 1. pobrać pozycję myszki na canvas
+        if (mouseEvent.getTarget() == canvas
+                && mouseEvent.getEventType() == MouseEvent.MOUSE_CLICKED
+                && mouseEvent.getButton() == MouseButton.PRIMARY) {
+            double x = mouseEvent.getX();
+            double y = mouseEvent.getY();
+            System.out.println("Kliknięto myszką: (" + x + ", " + y + ") ");
+            // 2. pobrać kolor z colorPicker
+            Color color = colorPicker.getValue();
+            // 3. pobrać promień z radiusSlider
+            double radius = radiusSlider.getValue();
+            // 4. narysować koło na canvas
+            canvas.getGraphicsContext2D().setFill(color); // ustawia kolor następnej akcji
+            canvas.getGraphicsContext2D().fillOval(x - radius, y - radius, radius*2, radius*2);
+        }
     }
 }
